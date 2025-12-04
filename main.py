ade014753f477ea2882fa10dd6a6ebc1cf16a0e7
kasimchoudhary <14co19@gmil.com>
2025-12-04 20:57:26 +0530

    added missing checks to skip the events and parsing of timestamp

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import os, json
+from datetime import datetime
 from typing import List, Dict, Any
 
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
+def parsed_timestamp(timestamp: str) -> (Any | None):
+    try:
+        # parse timestamp with Z value
+        return datetime.strptime(timestamp, DATE_FORMAT)
+    except:
+        return None
 
-def detect_missed_heartbeat(events: List[Dict[str, Any]], expected_interval_seconds: int, allowed_misses: int) -> List[Dict[str, str]]:
-    pass
+
+def detect_missed_heartbeat(events: List[Dict[str, Any]], expected_interval_seconds: int = 60, allowed_misses: int = 3) -> List[Dict[str, str]]:
+    # validate and group events based on service
+    grouped = dict()
+
+    for event in events:
+        service = event.get('service')
+        timestamp = event.get('timestamp')
+
+        # check service or timestamp
+        if not service or not timestamp:
+            continue
+
+        # check invalid timestamp
+        ptimestamp = parsed_timestamp(timestamp)
+        if not ptimestamp:
+            continue
+
+        grouped.setdefault(service, []).append(ptimestamp)
 
 
 if __name__ == "__main__":
@@ -18,4 +42,4 @@ if __name__ == "__main__":
     with open("events.json", "r", encoding="utf-8") as file:
         events = json.load(file)
 
-    detect_missed_heartbeat(events, 60, 3)
+    detect_missed_heartbeat(events)
